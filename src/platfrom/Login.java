910cff8b99103303f357ce88f48b3435a6cd8c0a
Ghislain KIMA <ghiskima@gmail.com>
2025-08-31 18:38:12 +0000

    Ajout de l'interface d'inscription

--- src/platfrom/Login.java
+++ src/platfrom/Login.java
@@ -19,40 +19,88 @@ public class Login extends HBox
 {
     private Scene scene ;
     private VBox rightVbox ;
+    private VBox leftVbox ;
+    private Button returnButton ;
+    private StackPane login ;
+    private Inscribe inscribe ;
 
     public Login()
     {
         super() ;
 
-        VBox leftVbox = new VBox() ;
+        leftVbox = new VBox() ;
         rightVbox = new VBox() ;
-        HBox.setHgrow(leftVbox, Priority.ALWAYS);
-        HBox.setHgrow(rightVbox, Priority.ALWAYS);
-        rightVbox.getChildren().add(login()) ;
-        rightVbox.setAlignment(Pos.CENTER);
+        returnButton = new Button("<") ;
+        inscribe = new Inscribe() ;
+
+        returnButton.setVisible(false) ;
+        login = (StackPane) defineLogin() ;
+        rightVbox.getChildren().addAll(new HBox(returnButton), login) ;
+        ((HBox)rightVbox.getChildren().get(0)).setPadding(new Insets(20, 0, 0, 20));
+        HBox.setHgrow(leftVbox, Priority.ALWAYS) ;
+        HBox.setHgrow(rightVbox, Priority.ALWAYS) ;
+        VBox.setVgrow(login, Priority.ALWAYS) ;
         this.getChildren().addAll(leftVbox, rightVbox) ;
         this.scene = new Scene(this) ;
+
         /* CSS Style */
         this.scene.getStylesheets().add("file:rssrc/styles/login.css") ;
         /* CSS Style */
+        
+        returnButton.setOnMouseClicked((event) ->
+        {
+            if (rightVbox.getChildren().contains(inscribe) && inscribe.choiceInOccurrence())
+            {
+                rightVbox.getChildren().removeLast() ;
+                rightVbox.getChildren().add(login) ;
+                returnButton.setVisible(false) ;
+            }
+            else
+            {
+                inscribe.defaultDefenition() ;
+            }
+        });
+        
+        /* Listener */
+        scene.widthProperty().addListener((obs, oldVal, newVal) ->
+        {
+            double w = newVal.doubleValue() ;
+            
+            if (w < 600)
+            {
+                this.getChildren().remove(leftVbox) ;
+            }
+            else
+            {
+                this.getChildren().clear() ;
+                this.getChildren().addAll(leftVbox, rightVbox) ;
+            }
+            if (this.getChildren().contains(leftVbox))
+            {
+                leftVbox.setPrefWidth(w*0.4) ;
+                leftVbox.setMinWidth(w*0.4) ;
+                leftVbox.setMaxWidth(w*0.4) ;
+            }
+        }) ;
+        /* Listener */
     }
     
-    private Parent login()
+    private Parent defineLogin()
     {
         int userLoginWidth = 320 ;
         int userLoginHeight = 400 ;
 
         StackPane stackPane = new StackPane() ;
-        VBox userLoginShadow = new VBox();
+        VBox userLoginShadow = new VBox() ;
         VBox userLogin = (VBox) defineUserLogin(userLoginWidth, userLoginHeight) ;
-        userLoginShadow.setMaxWidth(userLoginWidth) ;
-        userLoginShadow.setMinHeight(userLoginHeight) ;
+        userLoginShadow.setMinSize(userLoginWidth, userLoginHeight) ;
+        userLoginShadow.setMaxSize(userLoginWidth, userLoginHeight) ;
         stackPane.getChildren().addAll(userLoginShadow, userLogin);
         /* CSS Style */
-        userLogin.setId("userlogin") ;
+        userLogin.getStyleClass().add("userlogin") ;
         userLoginShadow.getStyleClass().add("userloginshadow") ;
         /* CSS Style */
-        
+
         return stackPane ;
     }
 
@@ -60,9 +108,9 @@ public class Login extends HBox
     {
         VBox userLogin = new VBox(15) ;
 
-        userLogin.setMinSize(userLoginWidth, userLoginHeight);
-        userLogin.setMaxSize(userLoginWidth, userLoginHeight);
-        userLogin.setPrefSize(userLoginWidth, userLoginHeight);
+        userLogin.setMinSize(userLoginWidth, userLoginHeight) ;
+        userLogin.setMaxSize(userLoginWidth, userLoginHeight) ;
+        userLogin.setPrefSize(userLoginWidth, userLoginHeight) ;
 
         VBox identifying = (VBox) labelNameAndTextField("Entrez votre identfiant") ;
         VBox password = (VBox) labelNameAndPasswordField("Mot de passe") ;
@@ -92,8 +140,10 @@ public class Login extends HBox
         /* Event */
         firstLaunch.getChildren().get(2).setOnMouseClicked((event) ->
         {
-            rightVbox.getChildren().remove(0) ;
-            rightVbox.getChildren().add(new Inscribe()) ;
+            returnButton.setVisible(true) ;
+            rightVbox.getChildren().removeLast() ;
+            rightVbox.getChildren().add(inscribe) ;
+            HBox.setHgrow(rightVbox, Priority.ALWAYS) ;
         }) ;
         remindFOrgetPassword.getChildren().get(2).setOnMouseClicked((event) ->
         {
@@ -104,10 +154,10 @@ public class Login extends HBox
         return userLogin ;
     }
 
-    public Parent labelNameAndTextField(String name)
+    public static Parent labelNameAndTextField(String name)
     { return new VBox(5, new Label(name), new TextField()) ; }
 
-    public Parent labelNameAndPasswordField(String name)
+    public static Parent labelNameAndPasswordField(String name)
     { return new VBox(5, new Label(name), new PasswordField()) ; }
 
     public Scene getMainLoginHboxScene()
